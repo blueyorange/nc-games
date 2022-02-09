371038510b5ddb87a5462ee40af5e061efa39c68
Russell <blueyorange@gmail.com>
2022-02-09 10:18:55 +0000

    more styling

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -2,11 +2,10 @@ export default function ReviewCard({review}) {
     const {title, category, owner, review_body, review_img_url} = review;
     return (
         <div className="ReviewCard">
-            <div className="ReviewCard__category">{category}</div>
+            <div className="ReviewCard__category">{category} </div>
             <h2 className="ReviewCard__title">{title}</h2>
             <img alt={title} src={review_img_url} className="ReviewCard__image"></img>
-            {/* <div className="ReviewCard__owner">{owner}</div> */}
-
+            <div className="ReviewCard__owner">review by <span className="ReviewCard__owner_name">{owner}</span></div>
             <div className="ReviewCard__body">{review_body}</div>
         </div>
     );
