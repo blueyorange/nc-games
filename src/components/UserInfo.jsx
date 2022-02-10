315522cb4e2b7749e65617554adb2188920e1e9b
Russell <blueyorange@gmail.com>
2022-02-10 09:37:05 +0000

    added UserInfo to user page

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -1,3 +1,12 @@
 export default function UserInfo({ user }) {
-  return <></>;
+  const { username, avatar_url, name } = user;
+  return (
+    <div className="UserInfo">
+      <img alt={name} src={avatar_url} className="UserInfo__image"></img>
+      <div className="UserInfo_names">
+        <h1 className="UserInfo__name">{name}</h1>
+        <div className="UserInfo__username">{username}</div>
+      </div>
+    </div>
+  );
 }
