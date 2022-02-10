025da38b66e71a7b20c048c7cb8c254b595e9a01
Russell <blueyorange@gmail.com>
2022-02-10 16:08:28 +0000

    added user context and default user

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
+import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import Reviews from "./components/Reviews";
 import UserPage from "./components/UserPage";
 import ReviewPage from "./components/ReviewPage";
+import { UserContext } from "./contexts/User";
 
 function App() {
+  const [user, setUser] = useState({
+    username: "jessjelly",
+    avatar_url:
+      "https://vignette.wikia.nocookie.net/mrmen/images/4/4f/MR_JELLY_4A.jpg/revision/latest?cb=20180104121141",
+    name: "Jess Jelly",
+  });
+
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Reviews />} />
-          <Route path="/reviews" element={<Reviews />} />
-          <Route path="/reviews/:category" element={<Reviews />} />
-          <Route path="/users/:username" element={<UserPage />} />
-          <Route path="/review/:review_id" element={<ReviewPage />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <UserContext.Provider value={(user, setUser)}>
+      <BrowserRouter>
+        <div className="App">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Reviews />} />
+            <Route path="/reviews" element={<Reviews />} />
+            <Route path="/reviews/:category" element={<Reviews />} />
+            <Route path="/users/:username" element={<UserPage />} />
+            <Route path="/review/:review_id" element={<ReviewPage />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 }
 
